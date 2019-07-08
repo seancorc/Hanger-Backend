d3c5334120cf8bceb061965f2e7aab177b366043
Sean Corcoran <seancorcoran45@icloud.com>
2019-07-07 18:59:08 -0700

    create and get users functionality

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from db import db, User
 
 app = Flask(__name__)
 
-db_filename = 'todo.db'
+db_filename = 'Hung.db'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,9 +16,30 @@ with app.app_context():
         db.create_all()
 
 
-@app.route('/helloworld')
+@app.route('/helloworld/')
 def helloworld():
-    return "Hello World!"
+        return "Hello World!"
+
+@app.route('/api/user/', methods=['POST'])
+def createUser():
+        postBody = json.loads(request.data)
+        email = postBody['email']
+        password = postBody['password']
+        username = postBody['username']
+        latitude = postBody['latitude']
+        longitude = postBody['longitude']
+        user = User(email = email, password = password, username = username, latitude = latitude, longitude = longitude)
+        db.session.add(user)
+        db.session.commit()
+        res = {'success': True, 'data': user.serialize()}
+        return json.dumps(res), 201
+
+@app.route('/api/users/')
+def getAllUsers():
+    allUsers = User.query.all()
+    res = {'success': True, 'data': [
+        user.serialize() for user in allUsers]}
+    return json.dumps(res), 200
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -8,14 +8,27 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
 
     def __init__(self, **kwargs):
         self.email = kwargs.get('email', '')
         self.password = kwargs.get('password', '')
+        self.username = kwargs.get('username', '')
+        self.latitude = kwargs.get('latitude', '')
+        self.longitude = kwargs.get('longitude', '')
 
     def serialize(self):
         return {
             'id': self.id,
             'email': self.email,
-            'password': self.password
+            'password': self.password,
+            'username': self.username,
+            'latitude': self.latitude,
+            'longitutde': self.longitude
         }
+
+    
+
+    
